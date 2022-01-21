5c5fd97baecc56f6a4108a8aa9180f709fd94b95
Rachel Bates <rachelbatesnj@gmail.com>
2022-01-20 20:42:08 -0500

    fixing form post request issue

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -10,8 +10,6 @@ const Posts = () => {
     const classes = useStyles();
     const posts = useSelector((state) => state.posts);
 
-    console.log(posts);
-    
     return (
         <div>
             <h1>Posts</h1>
